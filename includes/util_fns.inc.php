047c6782b8eb99df4db41d6f124edb1fde3782b0
idobook <44715125+idobook@users.noreply.github.com>
2019-05-25 01:30:13 -0500

    removing absolete ereg functions

--- includes/util_fns.inc.php
+++ includes/util_fns.inc.php
@@ -78,8 +78,7 @@ function is_valid_email($email)
     $local_array = explode(".", $email_array[0]);
 
     for ($i = 0; $i < sizeof($local_array); $i++) {
-        if(!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
-
+        if(!preg_match("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
             return false;
         }
     }
@@ -88,13 +87,13 @@ function is_valid_email($email)
 
     // checamos que sea un dominio valido
 
-    if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) {
+    if (!preg_match("^\[?[0-9\.]+\]?$", $email_array[1])) {
         $domain_array = explode(".", $email_array[1]);
         if (sizeof($domain_array) < 2) {
             return false;
         }
         for ($i = 0; $i < sizeof($domain_array); $i++) {
-            if(!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
+            if(!preg_match("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
                 return false;
             }
         }

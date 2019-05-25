3f56e0b63494040a4e0ee09167334741107e97f5
idobook <44715125+idobook@users.noreply.github.com>
2019-05-24 23:35:58 -0500

    updating encript functions based on new PHP version

--- encriptar.php
+++ encriptar.php
@@ -1,17 +1,20 @@
-<?php
-
-class Encrypter {
- 
-    private static $Key = "laguna";
- 
-    public static function encrypt ($input) {
-        $output = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5(Encrypter::$Key), $input, MCRYPT_MODE_CBC, md5(md5(Encrypter::$Key))));
-        return $output;
-    }
- 
-    public static function decrypt ($input) {
-        $output = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5(Encrypter::$Key), base64_decode($input), MCRYPT_MODE_CBC, md5(md5(Encrypter::$Key))), "\0");
-        return $output;
-    }
- 
-}
+<?php
+
+class Encrypter {
+
+    private static $Key = "laguna";
+
+    public static function encrypt ($input) {
+
+        //$output = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5(Encrypter::$Key), $input, MCRYPT_MODE_CBC, md5(md5(Encrypter::$Key))));
+        $encrypted = base64_encode(openssl_encrypt($input, AES-256-CBC, md5(Encrypter::$Key), $options = 0, $iv = "" ));
+        return $encrypted;
+    }
+
+    public static function decrypt ($input) {
+
+        //$output = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5(Encrypter::$Key), base64_decode($input), MCRYPT_MODE_CBC, md5(md5(Encrypter::$Key))), "\0");
+        $decrypted = openssl_decrypt(base64_decode($input), AES-256-CBC, md5(Encrypter::$Key), $options = 0, $iv = "");
+        return $decrypted;
+    }
+}

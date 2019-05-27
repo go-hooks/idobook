352b1783a87f9f13f55335aa97cc1b845f8cb315
idobook <44715125+idobook@users.noreply.github.com>
2019-05-26 19:28:25 -0500

    new main manu

--- includes/header2.php
+++ includes/header2.php
@@ -1,20 +1,6 @@
-<div id="navbar">
-  <a class="active" href="javascript:void(0)">Home</a>
-  <a href="javascript:void(0)">NOVIAS</a>
-  <a href="javascript:void(0)">NOVIOS</a>
-</div>
-
-<script>
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-</script>
+<ul id="mainmenu">
+  <li><a class="active" href="#home">INICIO</a></li>
+  <li><a href="#news">NOVIAS</a></li>
+  <li><a href="#contact">NOVIOS</a></li>
+  <li><a href="#about">PROVEEDORES</a></li>
+</ul>

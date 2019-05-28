5dd1ab564aba4f021fe6f6688c4364f0c0cd426a
idobook <44715125+idobook@users.noreply.github.com>
2019-05-28 12:43:05 -0500

    Adding main menu for testing

--- includes/header2.php
+++ includes/header2.php
@@ -3,3 +3,44 @@
                 <a class="menu-top-access" href="http://dev.idobook.com.mx/contacto.php">CONTÁCTANOS</a>
         </div>
 </div>
+
+<div id="menu" class="menu app-menu">
+    <div class="menu-wrapper">
+        <div class="menu-wrapper-align">
+            <div class="pure-g-r">
+                <div class="pure-u-1-5 main-logo">
+                                        <a title="Weddings">
+                        <img alt="Weddings" src="">
+                    </a>
+                </div>
+                <div class="pure-u-4-5">
+                    <div class="app-common-header-container" id="nav-main" role="navigation">
+                        <ul class="nav-main">
+                            <li class="nav-main-item">
+                                <a class="nav-main-link app-header-tab mr5 strong" data-tab="miboda">Planning Tools</a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link app-header-tab mr5 strong" data-tab="banquetes">Venues</a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link app-header-tab mr5 strong" data-tab="proveedores">Vendors</a>
+                            </li>
+                                    <li class="nav-main-item">
+                                    <a class="nav-main-link app-header-tab mr5 strong" data-tab="comunidad">Forums</a>
+                                </li>
+                                        <li class="nav-main-item mr5 strong">
+                                    <a class="nav-main-link app-header-tab show-caret-aside" data-tab="catalogo">Dresses</a>
+                                </li>
+                                        <li class="nav-main-item mr5 strong">
+                                    <a class="nav-main-link app-header-tab" data-tab="contenidos">Ideas</a>
+                                </li>
+                                <li class="nav-main-item mr5 strong">
+                                <a class="nav-main-link app-header-tab" data-tab="registry">Registry</a>
+                            </li>
+                            <li class="nav-main-item mr5 strong">
+                                <a class="nav-main-link app-header-tab" data-tab="websites">Wedding Website</a>
+                            </li>
+                        </ul>
+</div>
+                    
+                                        

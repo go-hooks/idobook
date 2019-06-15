28a5fe29791038e77a759e646a37855f8ed6a88c
idobook <44715125+idobook@users.noreply.github.com>
2019-06-15 12:15:20 -0500

    Adding inline menu

--- includes/header2.php
+++ includes/header2.php
@@ -4,18 +4,16 @@
             <a class="menu-top-access" href="http://dev.idobook.com.mx/contacto.php">CONTÁCTANOS</a>
         </div>
     </div>
-    <div class="menu-main">
-        <nav>
-            <ul class="wrapper">
-                <li><a id="home_icon" href="index.php"><img src="img/ico-home.png"></a></li>
-                <li><a id="menu_option_1" href="medicos.php">BANQUETES</a></li>
-                <li><a id="menu_option_2" href="hospitales.php">WEDDING PLANNERS</a></li>
-                <li><a id="menu_option_3" href="servicios-relacionados.php">JOYERÍAS</a></li>
-                <li><a id="menu_option_4" href="proveedores.php">PROVEEDORES</a></li>
-                <li><a id="menu_option_5" href="proveedores.php">NOVIAS</a></li>
-                <li><a id="menu_option_6" href="empresa.php">QUIÉNES SOMOS</a></li>               
-            </ul>
-        </nav>
+    <div class="menu-main"> 
+        <ul class="menu-main-wrapper">
+            <li><a id="menu-home" href="index.php">INICIO</a></li>
+            <li><a id="menu-option-1" href="medicos.php">BANQUETES</a></li>
+            <li><a id="menu-option-2" href="hospitales.php">WEDDING PLANNERS</a></li>
+            <li><a id="menu-option-3" href="servicios-relacionados.php">JOYERÍAS</a></li>
+            <li><a id="menu-option-4" href="proveedores.php">PROVEEDORES</a></li>
+            <li><a id="menu-option-5" href="proveedores.php">NOVIAS</a></li>
+            <li><a id="menu-option-6" href="empresa.php">QUIÉNES SOMOS</a></li>               
+        </ul>
     </div>
 </header>
                     
